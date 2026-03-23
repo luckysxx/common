3d0f6b588ef2062322332c3f58a733757c988e8a
luckys <2824602789@qq.com>
2026-03-23 16:02:34 +0800

    feat: 增加redis连接

---
+++ pkg/redis/redis.go
@@ -0,0 +1,36 @@
+package redis
+
+import (
+	"context"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+	"go.uber.org/zap"
+)
+
+// Config 定义了通用的 Redis 配置
+type Config struct {
+	Addr     string `mapstructure:"addr"`
+	Password string `mapstructure:"password"`
+	DB       int    `mapstructure:"db"`
+}
+
+// Init 根据配置初始化一个标准的 Redis 客户端
+func Init(cfg Config, log *zap.Logger) *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     cfg.Addr,
+		Password: cfg.Password,
+		DB:       cfg.DB,
+	})
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := client.Ping(ctx).Err(); err != nil {
+		log.Fatal("无法连接到 Redis", zap.Error(err))
+		return nil
+	}
+
+	log.Info("成功连接到 Redis")
+	return client
+}
